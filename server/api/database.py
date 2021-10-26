75d59a66d1ff9eb2cebf89b83c1742d1102fd507
Alexandros.Petkos <39966096+maestrakos@users.noreply.github.com>
2021-10-27 00:11:09 +0300

    Update database.py

--- server/api/database.py
+++ server/api/database.py
@@ -2,12 +2,15 @@ from sqlalchemy import create_engine
 from sqlalchemy.ext.declarative import declarative_base
 from sqlalchemy.orm import sessionmaker
 
+# Replace with your database username, password, hostname and db_name
 dbc = { "username": "", "password": "", "hostname": "", "database": "" }
 
+# Creates a database engine
 Engine = create_engine("mysql://" + 
 	dbc['username'] + ":" + dbc['password'] + "@" + 
 	dbc['hostname'] + "/" + dbc['database'] , pool_pre_ping=True)
 
+# Creates a session
 SessionLocal = sessionmaker(bind=Engine, autocommit=False, autoflush=False,)
 Base = declarative_base()
 
