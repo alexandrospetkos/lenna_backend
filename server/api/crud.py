ccbf4075bcf367979470f3c079fff3a9fcd223ed
Alexandros.Petkos <39966096+maestrakos@users.noreply.github.com>
2021-10-27 00:09:57 +0300

    Update crud.py

--- server/api/crud.py
+++ server/api/crud.py
@@ -4,14 +4,17 @@ from api import models
 
 # API
 
+# Get the user's id from his private key
 def get_user_from_key(db: Session, key: str):
 	return db.query(models.ApiKey).filter(models.ApiKey.private_key == key).first().user_id
 
+# Get the user's configuration
 def get_conf_from_user(db: Session, user_id: int):
 	return db.query(models.Conf).filter(models.Conf.user_id == user_id).first()
 
 # QG
 
+# Get user's query group ( an id indicating a collection of queries )
 def get_user_qgroup_collection(db: Session, user_id: int, qgroup_id: int, mode: int):
 	if mode == 0:
 		return db.query(func.max(models.QGroup.query_group)).filter(models.QGroup.user_id == user_id).first()
@@ -19,7 +22,8 @@ def get_user_qgroup_collection(db: Session, user_id: int, qgroup_id: int, mode: 
 		return db.query(models.QGroup).filter(models.QGroup.user_id == user_id, models.QGroup.query_group == qgroup_id).first()
 	elif mode == 2:
 		return db.query(models.QGroup).filter(models.QGroup.user_id == user_id).all()
-
+	
+# Create new user query group ( an id indicating a collection of queries )
 def new_user_qgroup_collection(db: Session, user_id: int, qgroup_id: int, time):
 	db_qgroup = models.QGroup(user_id=user_id, query_group=qgroup_id, time=time)
 	db.add(db_qgroup)
@@ -27,10 +31,12 @@ def new_user_qgroup_collection(db: Session, user_id: int, qgroup_id: int, time):
 	db.refresh(db_qgroup)
 	return db_qgroup
 
+# Get the content of a query group ( the queries associated with the group )
 def get_user_qgroup_content(db: Session, user_id: int, qgroup_id: int, n_memory: int):
 	return db.query(models.Query).filter(models.Query.user_id == user_id, 
 		models.Query.query_group == qgroup_id, models.Query.in_memory == 1).order_by(models.Query.query_id.desc()).limit(n_memory)
 
+# Create new query group content ( the queries associated with the group )
 def new_user_qgroup_content(db: Session, user_id: int, qgroup_id: int, request: str, response: str, in_memory: int, time):
 	db_query = models.Query(query_group=qgroup_id, request=request, response=response, 
 		in_memory=in_memory, time=time, user_id=user_id)
@@ -41,6 +47,7 @@ def new_user_qgroup_content(db: Session, user_id: int, qgroup_id: int, request: 
 
 # KB
 
+# Get the user's knowledge base 
 def get_user_kbase_content(db: Session, user_id: int, kbase: str):
 	if kbase.lower() == "default":
 		return db.query(models.KBase).filter(models.KBase.name == kbase.lower()).first().body
