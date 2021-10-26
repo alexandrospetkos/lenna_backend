d07a0d5b4a1525cbcc0b79a872e0a6c3b8827138
Alexandros.Petkos <39966096+maestrakos@users.noreply.github.com>
2021-10-27 00:06:22 +0300

    Update __init__.py

--- server/__init__.py
+++ server/__init__.py
@@ -1,8 +1,5 @@
 if __name__ == "__main__":
-    from api import main
+    from api import main # import the api script
 
-    import importlib
-    importlib.reload(main)
-
-    import uvicorn
+    import uvicorn # import and start the ASGI server
     uvicorn.run(main.app, host="127.0.0.1", port=8000, log_level="info")
